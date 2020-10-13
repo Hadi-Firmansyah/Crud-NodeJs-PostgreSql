f228bb88290eac2716ebc1ad39dd065f85ef2216
Petani Kode <hadifirmansyah.id@gmail.com>
2020-10-13 16:32:28 +0700

    Nambahin Query Database

---
+++ database.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE db_siswa;
+
+CREATE TABLE siswa(
+    id SERIAL PRIMARY KEY,
+    nisn VARCHAR(255),
+    nama VARCHAR(255),
+    jkl VARCHAR(255),
+    alamat VARCHAR(255),
+    kelas VARCHAR(255),
+    jurusan VARCHAR(255)
+);
